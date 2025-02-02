33c4721fc1f5cd6a8c91ba4e16de0dfca206b6b1
Yoann <maulineyoann@gmail.com>
2025-02-02 23:12:32 +0100

    first function expense

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,69 @@
-
-import Card from "./Components/Card";
-import SectionComponent from "./Containers/SctionComponent";
-
-import { useId } from "react";
-
-interface CardObjet {
-  id: number;
-  title: "Free" | "Premium";
-  price: number;
-}
+import { useId, useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState("");
+  const [expenses, setExpenses] = useState([]);
+  const [isExpense, setIsExpense] = useState(true);
 
-  const password = useId()
-  const optionCard: CardObjet[] = [
-    {
-      id: 0,
-      title: "Free",
-      price: 0,
-    },
-    {
-      id: 1,
-      title: "Premium",
-      price: 20,
-    },
-  ];
+  const expenseId = useId()
+
+  const amountValue = {
+    expense: "Dépense",
+    revenue: "Revenue"
+  }
+
+  const addTransaction = () => {
+    if (amount) {
+      const newTransaction = {
+        amount: parseFloat(amount),
+        type: isExpense ? amountValue.expense : amountValue.revenue,
+        id: Date.now(),
+      };
+      setExpenses([...expenses, newTransaction]);
+      setAmount("");
+    }
+  };
+
+  const totalExpenses = expenses
+  .filter((expense) => expense.type === amountValue.expense)
+  .reduce((acc, expense) => acc + expense.amount, 0)
+
+  const totalIncome = expenses
+  .filter((expense) => expense.type === amountValue.revenue)
+  .reduce((acc, expense) => acc + expense.amount, 0)
 
 
   return (
-    <>
-      <div className="h-screen bg-sky-950 text-white">
-        <SectionComponent>
-          <h1 className="text-center text-3xl">Nexa Finance</h1>
-          <div className="flex gap-10">
-            {optionCard.map((item) => (
-              <Card key={item.id} title={item.title} price={item.price} />
-            ))}
-          </div>
-            <label htmlFor={password}>Password
-            <input type="text" aria-describedby={password} />
-            </label>
 
-            <p id={password}>the password should contain at least 18 characters</p>
-        </SectionComponent>
-      </div>
-    </>
-  );
+    <div className="h-screen bg-sky-950 text-white">
+    <h1>Gestion de Budget</h1>
+    <div>
+      <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Entrez une somme" className="p-3 border" />
+      <select name="" id={expenseId} onChange={(e) => setIsExpense(e.target.value === "expense")} className="p-3 border">
+        {
+         Object.values(amountValue).map((item, index) => (
+            <option key={index}>{item}</option>
+          ))
+        }
+      </select>
+      <button onClick={addTransaction} className="border p-3 cursor-pointer">Ajouter</button>
+    </div>
+    <div>
+
+    <h3>Dépenses: {totalExpenses}</h3>
+    <h3>Revenue: {totalIncome}</h3>
+    <h3>Solde: {totalIncome - totalExpenses}</h3>
+    </div>
+    <h2>Hitoriques des Dépenses</h2>
+    <ul>
+      {
+        expenses.map((expense) => (
+          <li key={expense.id}>`${expense.type}: {expense.amount}`</li>
+        ))
+      }
+    </ul>
+  </div>
+  )
 }
 
 export default App;
